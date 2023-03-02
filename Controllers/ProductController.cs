b7eb8d8dedcdbe3e3fc773068899f3230bff8461
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-03-01 20:53:50 -0600

    Added product view from categories

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -10,5 +10,6 @@ namespace Northwind.Controllers
         public ProductController(NorthwindContext db) => _northwindContext = db;
 
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
+        public IActionResult Index(int id) => View(_northwindContext.Products.Where(p => p.Discontinued == false && p.CategoryId == id).OrderBy(p => p.ProductName));
     }
 }
