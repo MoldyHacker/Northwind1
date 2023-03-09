eca6941e6bf8ae216b72009d1baecb6b101081f0
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-03-08 18:23:00 -0600

    added discounts to database

--- Migrations/20230309001853_CreateDiscounts.Designer.cs
+++ Migrations/20230309001853_CreateDiscounts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Northwind1.Migrations
 {
     [DbContext(typeof(NorthwindContext))]
-    partial class NorthwindContextModelSnapshot : ModelSnapshot
+    [Migration("20230309001853_CreateDiscounts")]
+    partial class CreateDiscounts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/NorthwindContext.cs
+++ Models/NorthwindContext.cs
@@ -6,4 +6,5 @@ public class NorthwindContext : DbContext
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
 }
