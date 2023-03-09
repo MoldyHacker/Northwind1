fed1c2ec142d9e04e6bd8c2f9fb23578e3b26f4d
moldyhacker <56985918+MoldyHacker@users.noreply.github.com>
2023-03-08 18:56:25 -0600

    added discount and toast to Discounts page. and linked that page in the Layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,10 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Northwind.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index() => View();
-    }
+    // this controller depends on the NorthwindRepository
+    private NorthwindContext _dataContext;
+    public HomeController(NorthwindContext db) => _dataContext = db;
+    public ActionResult Index() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
 }
--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -11,5 +11,6 @@ namespace Northwind.Controllers
 
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
         public IActionResult Index(int id) => View(_northwindContext.Products.Where(p => p.Discontinued == false && p.CategoryId == id).OrderBy(p => p.ProductName));
+        public IActionResult Discounts() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now));
     }
 }
